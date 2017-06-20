fd9f7638f7af44f5e996842d69d827f57d33b18a
Nathan Lee <kwnath@gmail.com>
2017-06-20 13:10:01 +0800

    finished Lense CRUD (except views)

--- app/models/lense.rb
+++ app/models/lense.rb
@@ -3,10 +3,10 @@ class Lense < ApplicationRecord
   validates :price, presence: true
   validates :condition, presence: true
   validates :brandname, presence: true
-  validates :aperture_min, presence: true
-  validates :aperture_max, presence: true
-  validates :focal_length_min, presence: true
-  validates :focal_length_max, presence: true
+  validates :aperture_min, presence: true, numericality: { only_integer: true, less_than_or_equal_to: :aperture_max }
+  validates :aperture_max, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: :aperture_min }
+  validates :focal_length_min, presence: true, numericality: { only_integer: true, less_than_or_equal_to: :focal_length_max }
+  validates :focal_length_max, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: :focal_length_min }
   validates :image_stabilization, presence: true
   validates :mount_type, presence: true
   validates :camera_type, presence: true
