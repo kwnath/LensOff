2f3ee75d3460effe14f527215d5ada18b370916a
Nathan Lee <kwnath@gmail.com>
2017-06-20 12:53:51 +0800

    partial controller finished

--- app/models/lense.rb
+++ app/models/lense.rb
@@ -1,32 +1,40 @@
 class Lense < ApplicationRecord
-  # t.string   "name"
-  # t.text     "description"
-  # t.integer  "price"
-  # t.string   "condition"
-  acts_as_taggable_on :mount, :min_aperture, :max_aperture
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :condition, presence: true
+  validates :brandname, presence: true
+  validates :aperture_min, presence: true
+  validates :aperture_max, presence: true
+  validates :focal_length_min, presence: true
+  validates :focal_length_max, presence: true
+  validates :image_stabilization, presence: true
+  validates :mount_type, presence: true
+  validates :camera_type, presence: true
 
-  def mount_type
-    mount
-  end
+  # acts_as_taggable_on :mount, :min_aperture, :max_aperture
 
-  def min_ap
-    min_aperture_list.reduce.to_i
-  end
+  # def mount_type
+  #   mount
+  # end
 
-  def max_ap
-    max_aperture_list.reduce.to_i
-  end
+  # def min_ap
+  #   min_aperture_list.reduce.to_i
+  # end
 
-  def in_aperture_range?(number)
-    number.between? min_ap, max_ap
-  end
+  # def max_ap
+  #   max_aperture_list.reduce.to_i
+  # end
 
-  def self.in_aperture_range(number)
-    lenses = Lense.all
+  # def in_aperture_range?(number)
+  #   number.between? min_ap, max_ap
+  # end
 
-    lenses.select { |lense| lense.in_aperture_range?(number) }
+  # def self.in_aperture_range(number)
+  #   lenses = Lense.all
 
-  end
+  #   lenses.select { |lense| lense.in_aperture_range?(number) }
+
+  # end
 
 
 
