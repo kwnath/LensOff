5a670729fd0913166f5041316cbb9bbbc2fab285
Nathan Lee <kwnath@gmail.com>
2017-06-22 18:25:09 +0800

    fix to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :lense
-  validates :start_date, presence: true,
+  validates :start_date, presence: true
   validates :end_date, presence: true
   # Attach validations:
   # t.datetime "start_date"
